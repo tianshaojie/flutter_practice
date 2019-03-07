0658b90dea474a7fd425b43da3212c7fe9d2f52d
tianshaojie <tianshaojie@foundersc.com>
2019-03-07 12:59:11 +0800

    tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'FirstScreen.dart';
 
-
 void main() {
   runApp(new MaterialApp(
     title: 'Flutter',
@@ -10,40 +9,47 @@ void main() {
 }
 
 class Main extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        leading: new IconButton(
-            icon: new Icon(
-              Icons.camera_alt,
-              color: Colors.white,
-            ),
-            tooltip: 'Navigation Menu',
-            onPressed: null
-        ),
-        title: new Text('Flutter'),
-        actions: <Widget>[
-          new IconButton(
-              icon: new Icon(
-                Icons.search,
-                color: Colors.white,
+    return new DefaultTabController(
+        length: _allTabss.length,
+        child: new Scaffold(
+          appBar: PreferredSize(
+              child: new AppBar(
+                leading: new IconButton(
+                    icon: new Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ),
+                    tooltip: 'Navigation Menu',
+                    onPressed: null),
+                centerTitle: true,
+                title: new TabLayout(),
+                actions: <Widget>[
+                  new IconButton(
+                      icon: new Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      tooltip: 'Search',
+                      onPressed: null)
+                ],
               ),
-              tooltip: 'Search',
-              onPressed: null
-          )
-        ],
-      ),
-      body: new Center(
-        child: new Text('Hello world'),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        tooltip: 'Add',
-        child: new Icon(Icons.add),
-        onPressed: null
-      ),
+              preferredSize: Size.fromHeight(48.0)),
+        ));
+  }
+}
+
+class TabLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new TabBar(
+      isScrollable: true,
+      labelPadding: EdgeInsets.all(12.0),
+      indicatorSize: TabBarIndicatorSize.label,
+      tabs: _allTabss.map((String name) => new Tab(text: name)).toList(),
     );
   }
-
 }
+
+final List<String> _allTabss = <String>['全部', '港股', '美股', '沪深'];
