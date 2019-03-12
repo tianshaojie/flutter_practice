de6fd3d07c9f981e49678eb5bbe4f75e1b5e010f
tianshaojie <tianshaojie@foundersc.com>
2019-03-12 21:01:41 +0800

    splash

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -3,69 +3,6 @@ import 'package:flutter_practice/main/pages/main_home_screen.dart';
 import 'package:flutter_practice/main/pages/main_trade_screen.dart';
 import 'package:flutter_practice/main/pages/main_news_screen.dart';
 import 'package:flutter_practice/main/pages/main_mine_screen.dart';
-import 'package:flutter_practice/constants/constants.dart';
-
-void main() => runApp(MaterialApp(home: SplashPage()));
-
-class SplashPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => SplashState();
-}
-
-class SplashState extends State<SplashPage> with TickerProviderStateMixin {
-
-  Animation animation;
-  AnimationController controller;
-  AnimationStatusListener animationStatusListener;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // 动画管理器
-    controller = new AnimationController(
-        duration: new Duration(milliseconds: 1500),
-        vsync: this
-    );
-
-    // 动画
-    animation = Tween(begin: 1.0, end: 0.5).animate(controller);
-
-    animationStatusListener = (status) {
-      if(status == AnimationStatus.completed) {
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => MainPage()),
-                (router) => router == null
-        );
-      }
-    };
-
-    // 注册动画观察者
-    animation.addStatusListener((status) => animationStatusListener(status));
-
-    // 启动动画
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FadeTransition(
-      opacity: animation,
-      child: Image.asset(
-        splashBackgroundImage,
-        fit: BoxFit.fill,
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    controller.removeStatusListener(animationStatusListener);
-    controller.dispose();
-    super.dispose();
-  }
-
-}
 
 
 class MainPage extends StatelessWidget {
@@ -150,4 +87,3 @@ class _MainNavigationState extends State<MainNavigation>
     return _currentIndex == index ? Theme.of(context).primaryColor : Colors.grey;
   }
 }
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_practice/main.dart';
+import 'package:flutter_practice/main_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MainPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
