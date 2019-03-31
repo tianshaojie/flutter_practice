27992de2e983135a3b488540a444cfa916fbcb7d
tianshaojie <tianshaojie@foxmail.com>
2019-03-31 22:51:45 +0800

    Android 启屏图配置，避免启动白屏

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/splash_screen.dart';
+import 'package:flutter_practice/main_screen.dart';
 
-void main() => runApp(MaterialApp(home: SplashPage()));
+//void main() => runApp(MaterialApp(home: SplashPage()));
+void main() => runApp(MaterialApp(home: MainPage()));
 
