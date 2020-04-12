f011615633a1740ab0409d217ede348ff0a4cbd3
tianshaojie <tianshaojie@foxmail.com>
2020-04-12 20:06:56 +0800

    update spec

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tianshaojie/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tianshaojie/workspace/flutter_practice"
+export "FLUTTER_TARGET=/Users/tianshaojie/workspace/flutter_practice/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/tianshaojie/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
